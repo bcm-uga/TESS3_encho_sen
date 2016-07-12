be9f4a17c45ced7e54a03cf9adf6d5f4eb1cc236
Olivier Francois <Olivier.Francois@imag.fr>
2016-07-12 16:02:19 +0200

    barplotQ:order/example

--- R/plotQ.R
+++ R/plotQ.R
@@ -5,7 +5,6 @@
 #' ancestry coefficient matrix. Includes a sort-by-Q option.
 #' @title Barplot representation of a Q-matrix
 #' @author Kevin Caye, Olivier François
-#'
 #' @param Q an object of class \code{tess3Q} (Q matrix) containing a matrix of ancestry coefficients computed from \code{tess3} or converted from other program formats.
 #' @param sort.by.Q a Boolean value indicating whether individuals should be sorted by their ancestry level or not.
 #' @param col.palette is a list of color palettes. If \code{NULL}, a default list with 8 color palettes is used.
@@ -14,16 +13,18 @@
 #' @seealso \code{\link{plot.tess3Q}} \code{\link{as.qmatrix}} \code{\link{CreatePalette}}
 #' @examples
 #' data(data.at)
-#' obj <- tess3(data.at$X, coord = data.at$coord, K = 5, ploidy = 1, openMP.core.num = 4)
-#' Qmatrix <- obj$Q
-#' barplot(Qmatrix, border = NA, space = 0, xlab = "individuals", ylab = "Ancestry proportions", main = "Ancestry matrix")
+#' obj <- tess3Main(data.at$X, coord = data.at$coord, K = 5, ploidy = 1, openMP.core.num = 4)
+#' Q.matrix <- obj$Q
+#' ord = barplot(Q.matrix, border = NA, space = 0, xlab = "Individuals", ylab = "Ancestry proportions", main = "Ancestry matrix")
+#' axis(1, at = 1:170, labels = ord, las = 3, cex.axis = .6)
 #' @export
-barplot.tess3Q = function(Q, sort.by.Q = TRUE, col.palette = NULL, palette.length = 9, ...){
+barplot.tess3Q = function(Q, sort.by.Q = TRUE, col.palette = NULL, palette.length = 9, lab = FALSE, ...){
   if (class(Q)[1] != "tess3Q") {warning("Object Q not of class tess3Q.")}
   ## defines a default color palette (8 colors)
   if (is.null(col.palette)) {
-    if (ncol(Q) > 8)  stop("The default color palette expects less than 9 clusters.
-                           Use CreatePalette() to create color palettes for more than 8 clusters.")
+    cat("Use CreatePalette() to define color palettes.\n")
+    if (ncol(Q) > 8)
+      stop("The default color palette expects less than 9 clusters.")
     TestRequiredPkg("RColorBrewer")
     col.palette = list(
       c(RColorBrewer::brewer.pal(palette.length,"Reds")),
@@ -38,9 +39,9 @@ barplot.tess3Q = function(Q, sort.by.Q = TRUE, col.palette = NULL, palette.lengt
   }
   ## colors
   if (palette.length > 3){
-  mycol = sapply(col.palette, FUN = function(x) x[palette.length/2])}
+  colpal = sapply(col.palette, FUN = function(x) x[palette.length/2])}
   else {
-  mycol = sapply(col.palette, FUN = function(x) x[palette.length])
+  colpal = sapply(col.palette, FUN = function(x) x[palette.length])
   }
 
   if (sort.by.Q){
@@ -51,12 +52,13 @@ barplot.tess3Q = function(Q, sort.by.Q = TRUE, col.palette = NULL, palette.lengt
     or = order(gr)
     Qm = t(Q[or,])
     class(Qm) = "matrix"
-    barplot(Qm, col = mycol, ...)
+    barplot(Qm, col = colpal,...)
+    return(list(order = or))
     }
   else {
     Qm = t(Q)
     class(Qm) = "matrix"
-    barplot(Qm, col = mycol, ...)
+    barplot(Qm, col = colpal, ...)
   }
 }
 
@@ -66,7 +68,7 @@ barplot.tess3Q = function(Q, sort.by.Q = TRUE, col.palette = NULL, palette.lengt
 #'
 #' @param Q an object of class \code{tess3Q} containing a matrix of ancestry coefficients.
 #' @param
-#' @param col.palette is a list of color palettes. If \code{NULL}, a default list with 8 color palettes is used.
+#' @param col.palette a list of color palettes. If \code{NULL}, a default list with 8 color palettes is used.
 #' @param palette.length an integer value for the number of colors in each element of the palette list.
 #' @details details on interpolation methods
 #' \itemize{
@@ -76,11 +78,11 @@ barplot.tess3Q = function(Q, sort.by.Q = TRUE, col.palette = NULL, palette.lengt
 #' @return None
 #' @examples
 #' data(data.at)
-#' obj <- tess3(data.at$X, coord = data.at$coord, K = 5, ploidy = 1, openMP.core.num = 4)
+#' obj <- tess3Main(data.at$X, coord = data.at$coord, K = 5, ploidy = 1, openMP.core.num = 4)
 #' Qmatrix <- obj$Q
-#' plot(obj$Q, data.at$coord, method = "mapping.max", interpol = kriging(10), cex = .4, xlab = "Longitude", ylab= "Latitude", main = "Ancestry coefficients")
+#' plot(obj$Q, data.at$coord, method = "map.max", interpol = kriging(10), cex = .4, xlab = "Longitude", ylab= "Latitude", main = "Ancestry coefficients")
 #' @export
-plot.tess3Q <- function(Q, coord, method = "mapping.max", resolution = c(300,300), window = NULL,
+plot.tess3Q <- function(Q, coord, method = "map.max", resolution = c(300,300), window = NULL,
                         background = TRUE, raster.filename = NULL, interpolation.function = kriging(10),
                         col = NULL, col.palette = NULL, map = TRUE, palette.length = 9, ...) {
 
@@ -105,7 +107,7 @@ plot.tess3Q <- function(Q, coord, method = "mapping.max", resolution = c(300,300
       c(RColorBrewer::brewer.pal(palette.length,"Oranges"))
     )
   }
-  if (length(col.palette) < ncol(Q) & (method == "mapping.max" | method == "mapping.all")) {
+  if (length(col.palette) < ncol(Q) & (method == "map.max" | method == "map.all")) {
     stop("col.palette must of length ncol(Q)")
   }
 
@@ -126,14 +128,14 @@ plot.tess3Q <- function(Q, coord, method = "mapping.max", resolution = c(300,300
 
   if (method == "piechart") {
     PlotPiechartAncestryCoef(Q, coord, window, background, col, ...)
-  } else if (method == "mapping.max") {
+  } else if (method == "map.max") {
     message("Computing grid and background...")
     grid <- ComputeGridAndBackground(window, resolution, background, raster.filename)
     message("Interpolating maximum ancestry coefficients on a grid...")
     list.grid.z <- interpolation.function(Q, coord, grid$grid.x, grid$grid.y)
     message("Plotting the results...")
     PlotInterpotationMax(coord, list.grid.z, grid$grid.x, grid$grid.y, grid$background, col.palette, map, ...)
-  } else if (method == "mapping.all") {
+  } else if (method == "map.all") {
     message("Computing grid and background...")
     grid <- ComputeGridAndBackground(window, resolution, background, raster.filename)
     message("Interpolating all ancestry coefficients on a grid...")
