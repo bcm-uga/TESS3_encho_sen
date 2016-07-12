a7a01486a7a2bce7f5df9c837cf7cd53fec6effb
Olivier Francois <Olivier.Francois@imag.fr>
2016-07-12 09:39:45 +0200

    utils/tess2tess3/bugcorrection2

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,155 @@
+
+#' This function converts data sets imported from the STRUCTURE and the TESS 2.3 input file formats to the tess3 formats.
+#' @title Import input files from STRUCTURE and TESS 2.3
+#' @author Kevin Caye, Flora Jay, Olivier François
+#' @param dataframe a data frame read from a STRUCTURE or a TESS file.
+#' @param TESS a boolean value set to \code{TRUE} if the TESS 2.3 format is used. If \code{TRUE}, the
+#' geographic coordinates (Longitude, Latitude) must be binded left to the genetic marker matrix.
+#' @param diploid a boolean value set to \code{TRUE} for diploids and \code{FALSE} for haploids.
+#' @param FORMAT an integer value equal to 1 for markers encoded using one row of data for each individual, and
+#' \code{FORMAT = 2} for markers encoded using two rows of data for each individual.
+#' @param extra.row an integer value indicating the number of extra rows in the header of the input file (marker ids).
+#' @param extra.column an integer value indicating the number of extra columns in the input file. Extra columns come before the geographic coordinates
+#' in TESS 2.3 input files, and include individual ids, pop ids, phenotypes, etc. Geographic coordinates must be considered as extra columns if the flag \code{TESS}
+#' is set to \code{TESS = FALSE}. Missing data are encoded as "-9" or any negative values
+#' @return An object of class \code{list} containing a genotype matrix and individual geographic coordinates.
+#' @return Q
+#' @seealso \code{\link{tess3}}
+#' @examples
+#' ##
+#' @export
+tess2tess3 <- function(dataframe = NULL, TESS = TRUE, diploid = TRUE, FORMAT = 1, extra.row = 0, extra.column = 0){
+
+  if (!diploid & FORMAT == 2) stop("FORMAT = 2 is for diploids only.")
+
+  if (is.null(dataframe)) stop("dataframe cannot be NULL.")
+
+  if (!is.data.frame(dataframe)) stop("dataframe must be a data.frame object")
+
+  dat = dataframe
+
+  if (TESS == FALSE){
+    if (extra.row > 0) dat = dat[-(1:extra.row),]
+    if (extra.column > 0) dat = dat[,-(1:extra.column)]
+    n = dim(dat)[1]
+    L = dim(dat)[2]
+    if (FORMAT == 1 & diploid == FALSE) {n.ind = n; n.loc = L}
+    if (FORMAT == 1 & diploid == TRUE) {n.ind = n; n.loc = L/2}
+    if (FORMAT == 2 & diploid == TRUE) {n.ind = n/2; n.loc = L}
+    cat("Input file in the STRUCTURE format. The genotypic matrix has", n.ind, "individuals and", n.loc,
+        "markers.","\n")
+    cat("The number of extra rows is", extra.row,"and the number of extra columns is",extra.column,".\n")
+  }
+
+
+  if (TESS == TRUE){
+    if (extra.row > 0) dat = dat[-(1:extra.row),]
+    if (extra.column > 0) dat = dat[,-(1:extra.column)]
+
+    n = dim(dat)[1]
+    L = dim(dat)[2]
+
+    if (FORMAT == 1 & diploid == FALSE) {
+      n.ind = n; n.loc = L-2;
+      coord = dat[,1:2]
+      dat = dat[,-(1:2)]
+    }
+    if (FORMAT == 1 & diploid == TRUE) {
+      n.ind = n; n.loc = L/2 - 1;
+      coord = dat[,1:2]
+      dat = dat[,-(1:2)]
+    }
+    if (FORMAT == 2 & diploid == TRUE) {
+      n.ind = n/2; n.loc = L - 2;
+      coord = dat[seq(1,n,by = 2), 1:2]
+      dat = dat[,-(1:2)]
+    }
+    cat("Input file in the TESS format. The genotypic matrix has", n.ind, "individuals and", n.loc,
+        "markers.","\n")
+    cat("The number of extra rows is", extra.row,
+        "and the number of extra columns is",extra.column,".\n")
+  }
+
+  dat = as.matrix(dat)
+
+  unique.dat = unique(as.numeric(dat))
+  missing.dat = unique.dat[unique.dat < 0]
+
+  if (length(missing.dat) == 0)  cat("The input file contains no missing genotypes","\n")
+  if (length(missing.dat) == 1)  cat("Missing alleles are encoded as",missing.dat,"\n")
+  if (length(missing.dat) > 1) stop("Multiple values for missing data","\n")
+
+
+
+  # Convert allelic data into absence/presence data at each locus
+  # Results are stored in the "dat.binary" object
+
+  L = dim(dat)[2]
+
+  if (FORMAT == 1 & diploid == FALSE) {
+
+    dat.binary = NULL
+    for (j in 1:L){
+      allele = sort(unique(dat[,j]))
+      for (i in allele[allele >= 0]) dat.binary=cbind(dat.binary, dat[,j]== i )
+      LL = dim(dat.binary)[2]
+      ind = which(allele < 0)
+      if (length(ind) != 0){dat.binary[ind, (LL - length(allele) + 2):LL] = -9}
+    }}
+
+  if (FORMAT == 1 & diploid == TRUE) {
+    dat.2 = matrix(NA, ncol = L/2, nrow = 2*n)
+    for (ii in 1:n){
+      dat.2[2*ii-1,] = dat[ii, seq(1,L,by = 2)]
+      dat.2[2*ii,] = dat[ii,seq(2,L,by = 2)]
+    }
+    L = dim(dat.2)[2]
+
+    dat.binary = NULL
+
+    for (j in 1:L){
+      allele = sort(unique(dat.2[,j]))
+      for (i in allele[allele >= 0]) dat.binary=cbind(dat.binary, dat.2[,j]==i)
+      LL = dim(dat.binary)[2]
+      ind = which(allele < 0)
+      if (length(ind) != 0){dat.binary[ind, (LL - length(allele) + 2):LL] = -9}
+    }}
+
+
+
+  if (FORMAT == 2 & diploid == TRUE) {
+    dat.binary = NULL
+    for (j in 1:L){
+      allele = sort(unique(dat[,j]))
+      for (i in allele[allele >= 0]) dat.binary=cbind(dat.binary, dat[,j]==i)
+      LL = dim(dat.binary)[2]
+      ind = which(allele < 0)
+      if (length(ind) != 0){dat.binary[ind, (LL - length(allele) + 2):LL] = -9}
+    }}
+
+  # Compute a genotype count for each allele (0,1,2 or 9 for a missing value)
+  # The results are stored in 'genotype'
+
+  n = dim(dat.binary)[1]
+
+  if (diploid == TRUE){
+    n = n/2
+    genotype = matrix(NA,nrow=n,ncol=dim(dat.binary)[2])
+    for(i in 1:n){
+      genotype[i,]= dat.binary[2*i-1,]+dat.binary[2*i,]
+      genotype[i, (genotype[i,] < 0)] = NA
+    }}
+
+  if (FORMAT == 1 & diploid == FALSE){
+    genotype = dat.binary
+    for(i in 1:n){
+      genotype[i, (genotype[i,] < 0)] = NA
+    }}
+
+  return(list(X = genotype, coord = coord))
+}
+
+
 ###################################################
 #' This function tests Q matrix objects, and converts \code{matrix} objects into valid Q matrices
 #' @title Converts Q matrix
@@ -73,154 +225,4 @@ ComputeChi2AndPvalue <- function(Fst, K, n) {
   # compute adjusted p-values from the combined z-scores
   res$pvalue = as.numeric(pchisq(res$chi2 / res$gif, df = K - 1, lower.tail = FALSE))
   return(res)
-}
-
-
-#' This function converts data sets imported from the STRUCTURE and the TESS 2.3 input file formats to the tess3 formats.
-#' @title Import input files from STRUCTURE and TESS 2.3
-#' @author Kevin Caye, Flora Jay, Olivier François
-#' @param dataframe a data frame read from a STRUCTURE or a TESS file.
-#' @param TESS a boolean value set to \code{TRUE} if the TESS 2.3 format is used. If \code{TRUE}, the
-#' geographic coordinates (Longitude, Latitude) must be binded left to the genetic marker matrix.
-#' @param diploid a boolean value set to \code{TRUE} for diploids and \code{FALSE} for haploids.
-#' @param FORMAT an integer value equal to 1 for markers encoded using one row of data for each individual, and
-#' \code{FORMAT = 2} for markers encoded using two rows of data for each individual.
-#' @param extra.row an integer value indicating the number of extra rows in the header of the input file (marker ids).
-#' @param extra.column an integer value indicating the number of extra columns in the input file. Extra columns come before the geographic coordinates
-#' in TESS 2.3 input files, and include individual ids, pop ids, phenotypes, etc. Geographic coordinates must be considered as extra columns if the flag \code{TESS}
-#' is set to \code{TESS = FALSE}. Missing data are encoded as "-9" or any negative values
-#' @return An object of class \code{list} containing a genotype matrix and individual geographic coordinates.
-#' @return Q
-#' @seealso \code{\link{tess3}}
-#' @examples
-#' ##
-#'
-#'
-#'
-#' @export
-tess2tess3 <- function(dataframe = NULL, TESS = TRUE, diploid = TRUE, FORMAT = 1, extra.row = 0, extra.column = 0){
-
-  if (!diploid & FORMAT == 2) stop("FORMAT = 2 is for diploids only.")
-
-  dat = read.table(dataframe)
-
-  if (TESS == FALSE){
-    if (extra.row > 0) dat = dat[-(1:extra.row),]
-    if (extra.column > 0) dat = dat[,-(1:extra.column)]
-    n = dim(dat)[1]
-    L = dim(dat)[2]
-    if (FORMAT == 1 & diploid == FALSE) {n.ind = n; n.loc = L}
-    if (FORMAT == 1 & diploid == TRUE) {n.ind = n; n.loc = L/2}
-    if (FORMAT == 2 & diploid == TRUE) {n.ind = n/2; n.loc = L}
-    cat("Input file in the STRUCTURE format. The genotypic matrix has", n.ind, "individuals and", n.loc,
-        "markers.","\n")
-    cat("The number of extra rows is", extra.row,"and the number of extra columns is",extra.column,".\n")
-  }
-
-
-  if (TESS == TRUE){
-    if (extra.row > 0) dat = dat[-(1:extra.row),]
-    if (extra.col > 0) dat = dat[,-(1:extra.col)]
-
-    n = dim(dat)[1]
-    L = dim(dat)[2]
-
-    if (FORMAT == 1 & diploid == FALSE) {
-      n.ind = n; n.loc = L-2;
-      coord = dat[,1:2]
-      dat = dat[,-(1:2)]
-    }
-    if (FORMAT == 1 & diploid == TRUE) {
-      n.ind = n; n.loc = L/2 - 1;
-      coord = dat[,1:2]
-      dat = dat[,-(1:2)]
-    }
-    if (FORMAT == 2 & diploid == TRUE) {
-      n.ind = n/2; n.loc = L - 2;
-      coord = dat[seq(1,n,by = 2), 1:2]
-      dat = dat[,-(1:2)]
-    }
-    cat("Input file in the TESS format. The genotypic matrix has", n.ind, "individuals and", n.loc,
-        "markers.","\n")
-    cat("The number of extra rows is", extra.row,
-        "and the number of extra columns is",extra.columns,".\n")
-  }
-
-  dat = as.matrix(dat)
-
-  unique.dat = unique(as.numeric(dat))
-  missing.dat = unique.dat[unique.dat < 0]
-
-  if (length(missing.dat) == 0)  cat("The input file contains no missing genotypes","\n")
-  if (length(missing.dat) == 1)  cat("Missing alleles are encoded as",missing.dat,"\n")
-  if (length(missing.dat) > 1) stop("Multiple values for missing data","\n")
-
-
-
-  # Convert allelic data into absence/presence data at each locus
-  # Results are stored in the "dat.binary" object
-
-  L = dim(dat)[2]
-
-  if (FORMAT == 1 & diploid == FALSE) {
-
-    dat.binary = NULL
-    for (j in 1:L){
-      allele = sort(unique(dat[,j]))
-      for (i in allele[allele >= 0]) dat.binary=cbind(dat.binary, dat[,j]== i )
-      LL = dim(dat.binary)[2]
-      ind = which(allele < 0)
-      if (length(ind) != 0){dat.binary[ind, (LL - length(allele) + 2):LL] = -9}
-    }}
-
-  if (FORMAT == 1 & diploid == TRUE) {
-    dat.2 = matrix(NA, ncol = L/2, nrow = 2*n)
-    for (ii in 1:n){
-      dat.2[2*ii-1,] = dat[ii, seq(1,L,by = 2)]
-      dat.2[2*ii,] = dat[ii,seq(2,L,by = 2)]
-    }
-    L = dim(dat.2)[2]
-
-    dat.binary = NULL
-
-    for (j in 1:L){
-      allele = sort(unique(dat.2[,j]))
-      for (i in allele[allele >= 0]) dat.binary=cbind(dat.binary, dat.2[,j]==i)
-      LL = dim(dat.binary)[2]
-      ind = which(allele < 0)
-      if (length(ind) != 0){dat.binary[ind, (LL - length(allele) + 2):LL] = -9}
-    }}
-
-
-
-  if (FORMAT == 2 & diploid == TRUE) {
-    dat.binary = NULL
-    for (j in 1:L){
-      allele = sort(unique(dat[,j]))
-      for (i in allele[allele >= 0]) dat.binary=cbind(dat.binary, dat[,j]==i)
-      LL = dim(dat.binary)[2]
-      ind = which(allele < 0)
-      if (length(ind) != 0){dat.binary[ind, (LL - length(allele) + 2):LL] = -9}
-    }}
-
-  # Compute a genotype count for each allele (0,1,2 or 9 for a missing value)
-  # The results are stored in 'genotype'
-
-  n = dim(dat.binary)[1]
-
-  if (diploid == TRUE){
-    n = n/2
-    genotype = matrix(NA,nrow=n,ncol=dim(dat.binary)[2])
-    for(i in 1:n){
-      genotype[i,]= dat.binary[2*i-1,]+dat.binary[2*i,]
-      genotype[i, (genotype[i,] < 0)] = NA
-    }}
-
-    if (FORMAT == 1 & diploid == FALSE){
-    genotype = dat.binary
-    for(i in 1:n){
-      genotype[i, (genotype[i,] < 0)] = NA
-    }}
-
-  return(list(X = genotype, coord = coord))
 }
