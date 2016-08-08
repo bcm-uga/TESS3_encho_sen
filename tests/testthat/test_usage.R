ebe6638b9868a38f2ee3d2dad8a957e91c17c5d9
Kevin Caye <cayek@timc-bcm-29p.local>
2016-08-08 13:55:09 +0200

    gette

---
+++ tests/testthat/test_usage.R
@@ -0,0 +1,17 @@
+context("Usage")
+
+
+test_that("Minimal example", {
+  data("data.for.test", package = "tess3r")
+  obj <- tess3(X = data.for.test$X,
+               coord = data.for.test$coord,
+               K = 1:6,
+               ploidy = 1)
+  plot(obj) #plot l'erreur de CV, choix de K
+  q.matrix <- qmatrix(obj, K = 3) #get Q
+  expect_equal(dim(q.matrix), c(data.for.test$n, 3))
+  barplot(q.matrix, K = 3)
+  plot(q.matrix, data.for.test$coord, K=3) #map les coeffs par krig
+  p.values = pvalue(obj, K=3) #get les pvaleurs
+  expect_equal(dim(p.values), c(data.for.test$L, 1))
+})
