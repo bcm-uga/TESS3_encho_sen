3b4416b3c07be3b910d9f6ef6342e8cbf3040952
Flora Jay <florajayf@gmail.com>
2016-10-19 12:31:21 +0200

    minimal tes adding piechart + need background=F + remove param K warning

--- tests/testthat/test_usage.R
+++ tests/testthat/test_usage.R
@@ -10,8 +10,13 @@ test_that("Minimal example", {
   plot(obj) #plot l'erreur de CV, choix de K
   q.matrix <- qmatrix(obj, K = 3) #get Q
   expect_equal(dim(q.matrix), c(data.for.test$n, 3))
-  barplot(q.matrix, K = 3)
-  plot(q.matrix, data.for.test$coord, K=3) #map les coeffs par krig
+  barplot(q.matrix)
+  plot(q.matrix, data.for.test$coord) #map les coeffs par krig
+  # FJ: previous line does not work for me with backgound=T because it's in the sea, adding following:
+  plot(q.matrix, data.for.test$coord, background = FALSE)
   p.values = pvalue(obj, K=3) #get les pvaleurs
   expect_equal(dim(p.values), c(data.for.test$L, 1))
-})
+  piechart(q.matrix, data.for.test$coord) #map les coeffs par krig
+
+
+  })
