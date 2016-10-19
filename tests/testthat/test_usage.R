06b8374fd02d624923afb8c5df0bfd8a6a2247a8
Flora Jay <florajayf@gmail.com>
2016-10-19 18:30:58 +0200

    test_usage corrected version

--- tests/testthat/test_usage.R
+++ tests/testthat/test_usage.R
@@ -11,9 +11,7 @@ test_that("Minimal example", {
   q.matrix <- qmatrix(obj, K = 3) #get Q
   expect_equal(dim(q.matrix), c(data.for.test$n, 3))
   barplot(q.matrix)
-  plot(q.matrix, data.for.test$coord) #map les coeffs par krig
-  # FJ: previous line does not work for me with backgound=T because it's in the sea, adding following:
-  plot(q.matrix, data.for.test$coord, background = FALSE)
+  plot(q.matrix, data.for.test$coord, background = FALSE)  #map les coeffs par krig
   p.values = pvalue(obj, K=3) #get les pvaleurs
   expect_equal(dim(p.values), c(data.for.test$L, 1))
   piechart(q.matrix, data.for.test$coord) #map les coeffs par krig
